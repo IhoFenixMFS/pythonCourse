ad7063210400401cfd1072b599a86fd52f8b5c81
IhoFenixMFS <iho-ladamadeltiempo@live.com>
2021-01-18 11:25:09 +0100

    Python course 2nd week exercises

--- diccionarios1.py
+++ diccionarios1.py
@@ -1,4 +1,9 @@
-def diccionarios1(l):
+def diccionarios1(lista):
+    print(lista)
+    dic={l[0] : l[1:] for l in lista}
+    return dic
+
+def diccionarios1extra(l):
     dic={}
     print(l)
     for x in l:
@@ -7,16 +12,7 @@ def diccionarios1(l):
     	dic[key]=values
     return dic
 
-def diccionarios1ex(l):
-    dic={}
-    print(l)
-    for x in l:
-    	key=x[0]
-    	values=list(x[1:])
-    	dic[key]=values
-    return dic
-
-def diccionariosExtra1(l):
+def extraDicc1(l):
 	dic={}
 	k=l[0]
 	v=l[1]
@@ -27,15 +23,14 @@ def diccionariosExtra1(l):
 			dic[k[x]]=""
 	print(dic)
 
-def diccionariosExtra2(l):
+def extraDicc2(l):
     keys=l[0]
     values=l[1]
     dic= dict(zip(keys,values))
     return dic
-
 	
 #lista = [("a","b","c","d"),("A","B","C","D")]
-#lista = [(1,2,3), ("A","B","C")]
-lista = [(1,2),(3,4)]
+#lista = [(1,2),(3,4)]
+lista = [(1,2,3), ("A","B","C",4)]
 print(diccionarios1(lista))
 print()
--- dicionarios2.py
+++ dicionarios2.py
@@ -1,7 +1,16 @@
-def diccionarios2(l):
+def diccionarios1(cadena):
+    print(cadena)
+    #se deja bonito
+    cadena=cadena.replace('¡' ,'' ).replace('!' ,'' ).replace('#' ,'' ).replace('@' ,'' ).replace('€','').replace('¿' ,'' ).replace('?' ,'' ).replace(',' ,'' ).replace('.' ,'' )
+    #se pasa a minúsculas y se crea una lista de palabras
+    lista=cadena.lower().split()
+    #se pone cada palabra como clave y su número de apariciones como valor
+    dic={l : lista.count(l) for l in lista}
+    return dic
 
-    return l
 
-
-lista = []
-print(diccionarios2(lista))
+#cadena = "Una cadena y una prueba"
+#cadena = "#qUITo@ ¡varias@! ¿co€sas? varias."
+cadena = "Mi casa, mi coche y mi otro coche"
+print(diccionarios1(cadena))
+print()
